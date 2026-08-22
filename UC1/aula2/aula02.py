730605c65dffba6c375bdb4d6ef06ad34191837b
jorgeallbeertto26 <jorgeallbertto@yahoo.com.br>
2026-08-22 00:10:32 -0300

    Add files via upload

---
+++ UC1/aula2/aula02.py
@@ -0,0 +1,34 @@
+
+#nome = "Joao"
+#idade = 25
+
+#print  (nome,"-", idade)
+
+#a = 10
+#b = 5
+
+#print (f"Soma: {a + b} \n")
+#print (f"Subtração: ", a - b)
+#print (f"Multiplicaçao: ",  a * b)
+#print (f"Divisão: ",  a / b)
+
+#primeiro_nome = "Maria" 
+#segundo_nome = "Silva"
+#nome_completo = primeiro_nome + " " + segundo_nome
+#print (nome_completo)
+
+# x = 5
+# y = 20
+# print ("x é maior que y?", x > y)
+# print ("x é igual que y?", x == y)
+
+# if x > y:
+#     print ("é maior")
+# elif x == y:
+#     print ("é igual")
+# else: 
+#     print ("é menor")
+
+
+
+       
---
+++ UC1/aula2/aula02_calculadora.py
@@ -0,0 +1,55 @@
+##### ESTRUTURAS DE DECISAO - Decisões Múltiplas (elif e else):
+
+# valor_analisado = 15
+# limite_inferior = 10
+
+# if valor_analisado > limite_inferior:
+#     print ("O valor esta acima do limite. Sinalize o dado")
+
+##### ESTRUTURAS DE DECISAO - Operadores de Comparação
+# tipo_transacao = "compra"
+
+# if tipo_transacao == "venda":
+#     print ("Processamento de receita")
+# elif tipo_transacao == "compra":
+#     print ("Processamento de despesa e inventario")
+# else:
+#     print("Tipo de transacao de desconhecido. Requer auditoria.")
+
+##### ESTRUTURAS DE DECISAO - Operadores de Comparação
+# registro_id = 95
+# status_registro = "Novo"
+
+# if status_registro == "Novo":
+#     print ("O registro é novo.")
+
+# if registro_id < 100 and registro_id >0:
+#     print ("ID valido no intervalo 1 a  99")
+
+
+# ##### OPERADORES LOGICOS DE TABELA VERDADE
+# volume = 12500
+# probabilidade = 0.90
+
+# if volume > 10000 and probabilidade > 0.8:
+#     print ("ALERTA! Risco Alto! Requer intervenção imediata!")
+# else:
+#     print ("Risco controlado")
+
+
+# #####Combinando Decisões
+# valor_dado = 500
+# status_dado = "Normal"
+
+# if valor_dado < 1000 or status_dado == "Critico":
+#     print("Amostra importante para análise.")
+# else:
+#     print("Amostra padrão")
+
+# if not(valor_dado > 1000 and status_dado == "Normal"):
+#     print("A amostra não é um 'dado padrão alto'.")
+
+###CALCULADORA
+# 
+num1 
+num2          
