f6f834fa6454b3989137383e998bc2e9dd32faa8
jorgeallbeertto26 <jorgeallbertto@yahoo.com.br>
2026-08-22 00:03:08 -0300

    Delete UC1/cp phelipe directory

--- UC1/cp phelipe/exercicios/aula01/01-boletim/boletim.py
+++
@@ -1,7 +0,0 @@
-name = input("Insira o nome do aluno: ")
-first_grade = float(input("Insira a primeira nota: "))
-second_grade = float(input("Insira a segunda nota: "))
-
-average = (first_grade + second_grade) / 2
-
-print(f"A nota de {name} na primeira avaliação foi {first_grade} e na segunda avaliação foi {second_grade}, portanto sua média é {average:.1f}")
