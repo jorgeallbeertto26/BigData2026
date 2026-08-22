54ce08c5b6611ccb3a8e0be790ff5ef5f9e11b78
jorgeallbeertto26 <jorgeallbertto@yahoo.com.br>
2026-08-21 23:44:15 -0300

    Add files via upload

---
+++ desafio_02_aula01.py
@@ -0,0 +1,14 @@
+nota_1 = 5
+nota_2 = 6
+nota_3 = 2
+nota_4 = 3
+media = (nota_1 + nota_2 + nota_3 + nota_4) / 4
+
+print(media)
+if media > 7:
+    print ("Aprovado")
+elif media >= 5 and media <= 7:
+    print ("Recuperação")
+else:
+    print("Reprovado")
+
